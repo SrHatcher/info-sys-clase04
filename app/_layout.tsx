dba85c1e82f9e636530480489fa0b7c0c06ee842
SrHatcher <erickomaldonado11@gmail.com>
2025-02-27 01:46:19 +0000

    clase del 26 de febrero

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,29 @@
 import { View, Text } from 'react-native'
 import React, { useEffect } from 'react'
 import { Slot, SplashScreen } from 'expo-router'
+import {GestureHandlerRootView} from 'react-native-gesture-handler'
 import { useFonts } from 'expo-font'
 import "./global.css"
 
 const RootLayout = () => {
     SplashScreen.preventAutoHideAsync();
 
-    const [fonsLoaded, error]=useFonts({
+    const [fontsLoaded, error]=useFonts({
         'WorkSans-Black': require('../assets/fonts/WorkSans-Black.ttf'),
         'WorkSans-Light': require('../assets/fonts/WorkSans-Light.ttf'),
         'WorkSans-Medium': require('../assets/fonts/WorkSans-Medium.ttf'),
     })
 
     useEffect(()=>{
+      if(error) throw error;
+      if (fontsLoaded) SplashScreen.hideAsync();
+    }, [fontsLoaded, error]);
 
-    })
-
+    if (!fontsLoaded && !error) return null;
   return (
-    <Slot />
+    <GestureHandlerRootView className='flex flex-1'>
+      <Slot />
+    </GestureHandlerRootView>
   )
 }
 
--- app/drawer/_layout.tsx
+++ app/drawer/_layout.tsx
@@ -1,11 +1,38 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import Drawer from 'expo-router/drawer'
+import { Ionicons } from '@expo/vector-icons'
+import CustomDrawer from '@/components/shared/CustomDrawer'
 
 const DrawerLayout = () => {
   return (
-    <View>
-      <Text>holaaa</Text>
-    </View>
+    <Drawer
+      drawerContent={CustomDrawer}
+      screenOptions={{
+      overlayColor: 'rgba(0,0,0,6)',
+      drawerActiveTintColor: 'indigo',
+      headerShadowVisible: false,
+      sceneStyle: {
+        backgroundColor: "white"
+      }
+    }}>
+      <Drawer.Screen 
+        name='user/index'
+        options={{
+          drawerLabel: "Usuarios",
+          title: "Usuarios",
+          drawerIcon: ({color, size})=><Ionicons name='person' size={size} color={color}/> 
+        }}
+      />
+      <Drawer.Screen 
+        name='schedule/index'
+        options={{
+          drawerLabel: "Horario",
+          title: "Horario",
+          drawerIcon: ({color, size})=><Ionicons name='calendar' size={size} color={color}/> 
+        }}
+      />
+    </Drawer>
   )
 }
 
